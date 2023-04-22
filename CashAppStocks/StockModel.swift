8416b5185a49c65ffade276dd8e8c805049a5744
Alphonso Sensley II <alphonso.sensleyjr@gmail.com>
2023-04-21 22:14:56 -0700

    Added initial Model based on endpoint JSON response.

---
+++ CashAppStocks/StockModel.swift
@@ -0,0 +1,30 @@
+//
+//  StockModel.swift
+//  CashAppStocks
+//
+//  Created by Alphonso Sensley II on 4/21/23.
+//
+
+import Foundation
+
+struct StockData: Codable {
+    let stocks: [Stock]
+}
+
+struct Stock: Codable {
+    let ticker: String
+    let name: String
+    let currency: String
+    let currentPriceCents: Int
+    let quantity: Int?
+    let currentPriceTimestamp: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case ticker
+        case name
+        case currency
+        case currentPriceCents = "current_price_cents"
+        case quantity
+        case currentPriceTimestamp = "current_price_timestamp"
+    }
+}
